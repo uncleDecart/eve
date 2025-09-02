beee5859c8d982b40bed69380b77edf7e949bee5
Pavel Abramov <uncle.decart@gmail.com>
2025-09-02 14:47:42 +0200

    keep

--- pkg/pillar/pubsub/nkvdriver/driver.go
+++ pkg/pillar/pubsub/nkvdriver/driver.go
@@ -62,7 +62,7 @@ func (d *NkvDriver) Publisher(global bool, name, topic string, persistent bool, 
 
 func (s *NkvDriver) pubDirName(name string) string {
 	if name != "" {
-		return fmt.Sprintf("default.%s", name)
+		return fmt.Sprintf("runtime.%s", name)
 	} else {
 		return "default"
 	}
@@ -70,7 +70,7 @@ func (s *NkvDriver) pubDirName(name string) string {
 
 func (s *NkvDriver) fixedDirName(name string) string {
 	if name != "" {
-		return fmt.Sprintf("runtime.%s", name)
+		return fmt.Sprintf("runtime.fixeddir.%s", name)
 	} else {
 		return "runtime"
 	}
